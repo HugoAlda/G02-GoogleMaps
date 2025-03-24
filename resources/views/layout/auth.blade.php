0d82a5525c6834ab665bf8361a32b584e0cfeb29
adrimartin6 <adrianmartincalvet@gmail.com>
2025-03-24 16:05:13 +0100

    Layout y vista de lobby empezados

--- resources/views/layout/auth.blade.php
+++ resources/views/layout/auth.blade.php
@@ -23,8 +23,7 @@
     {{-- Scripts de Bootstrap --}}
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 
-    {{-- Scripts personalizados --}}
-    <script src="{{ asset('js/auth/utils.js') }}"></script>
+    {{-- Script de sweet alert  --}}
     @stack('scripts')
 </body>
 </html>
